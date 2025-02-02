839e632f1251e3b690bb60f5af31a904be668280
Ernest <61914660+muiboonyang@users.noreply.github.com>
2025-02-03 01:45:52 +0800

    Update layout.tsx

--- next-js/src/app/layout.tsx
+++ next-js/src/app/layout.tsx
@@ -89,7 +89,7 @@ export default function Layout({ children }: LayoutProps) {
 
                   <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                     <div className="flex-shrink-0 flex items-center">
-                      <a href="/next-js/public">
+                      <a href="/">
                         <img
                           className="block lg:hidden h-8 w-auto"
                           src={
